4b71ba3c72b2e8eabfe3ee1302d422683968169e
devilyouwei@foxmail.com <devilyouwei@foxmail.com@0e1a2743-8d03-4e5a-a60c-7e26af42b145>
2019-06-17 13:25:13 +0000

    beta5
    git-svn-id: svn://svn4.apicloud.com/A6004592205468/%E8%81%9A%E5%90%88%E8%A7%86%E9%A2%91@21 0e1a2743-8d03-4e5a-a60c-7e26af42b145

--- script/app.js
+++ script/app.js
@@ -294,7 +294,7 @@ window.$ = {
             if (ret) {
                 var result = ret.result;
                 if (result.update == true && result.closed == false) {
-                    var str = '新版本型号:&nbsp;' + result.version + '<br>' + result.updateTip + '<br>发布时间:' + result.time;
+                    var str = '新版本型号:&nbsp;' + result.version + '<br>' + result.updateTip + '<br><br>发布时间:' + result.time;
                     api.confirm({
                         title: '新版本',
                         msg: str,
